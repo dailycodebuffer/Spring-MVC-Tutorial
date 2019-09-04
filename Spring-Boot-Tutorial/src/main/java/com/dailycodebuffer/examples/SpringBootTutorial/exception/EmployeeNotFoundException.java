8ef3f7107b03f626f1ff544e94750fcf502c80f3
DailyCodeBuffer <dailycodebuffer@gmail.com>
2019-09-04 22:48:18 +0530

    Spring boot tutorial initia setup
    Spring boot tutorial initia setup. Introduction

---
+++ Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/examples/SpringBootTutorial/exception/EmployeeNotFoundException.java
@@ -0,0 +1,14 @@
+package com.dailycodebuffer.examples.SpringBootTutorial.exception;
+
+public class EmployeeNotFoundException extends  RuntimeException {
+
+    public EmployeeNotFoundException(String message, Throwable cause)
+    {
+        super(message,cause);
+    }
+
+    public EmployeeNotFoundException()
+    {
+        super();
+    }
+}
